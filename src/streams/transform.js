7f7fef2838b6524856c5a0571620300a155c1ebd
Igor Moskalev <moskalev.igor.v@gmail.com>
2024-01-30 03:55:02 +0600

    feat: implement stream operations for file read, write, and text transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const transformStream = new Transform({
+        transform(chunk, _, callback) {
+            const reversedChunk = chunk.toString().split('').reverse().join('');
+            callback(null, reversedChunk);
+        }
+    })
+
+    process.stdin.pipe(transformStream).pipe(process.stdout);
+
+    return new Promise((resolve, reject) => {
+        transformStream.on('finish', () => {
+            resolve();
+        });
+        transformStream.on('error', (err) => {
+            reject(err);
+        });
+    })
 };
 
 await transform();
