e8fac079183a31dbe74a9ae9052f472e635cf8a5
Igor Moskalev <moskalev.igor.v@gmail.com>
2024-01-30 05:41:43 +0600

    feat: implement child process creation from file script.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,13 @@
+import { fork } from 'child_process';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const cp = fork(join(__dirname, 'files', 'script.js'), args, { silent: true });
+
+    process.stdin.pipe(cp.stdin);
+    cp.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
