01a4685ea18bed6e80b2f30d5844416242913880
Igor Moskalev <moskalev.igor.v@gmail.com>
2024-01-30 04:16:08 +0600

    feat: add file compression and decompression functionality using zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const compress = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const srcFilename = join(__dirname, 'files', 'fileToCompress.txt');
+    const dstFilename = join(__dirname, 'files', 'archive.gz');
+
+    const source = createReadStream(srcFilename);
+    const destination = createWriteStream(dstFilename);
+    const gzip = createGzip();
+
+    source.pipe(gzip).pipe(destination);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const decompress = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const srcFilename = join(__dirname, 'files', 'archive.gz');
+    const dstFilename = join(__dirname, 'files', 'fileToCompress.txt');
+
+    const source = createReadStream(srcFilename);
+    const destination = createWriteStream(dstFilename);
+    const gunzip = createGunzip();
+
+    source.pipe(gunzip).pipe(destination);
 };
 
 await decompress();
