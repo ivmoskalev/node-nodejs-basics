6ea22b2b3c843bbee9362c46c2188c94cdecb814
Igor Moskalev <moskalev.igor.v@gmail.com>
2024-01-30 05:24:35 +0600

    feat: implement worker threads for fibonacci calculation

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,25 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+    const numCores = os.cpus().length;
+    let workers = [];
+    // let results = new Array(numCores);
+    let startIndex = 10;
+    const workerUrl = new URL('./worker.js', import.meta.url);
+
+    const calculateFibonacci = (workerData) => new Promise((resolve) => {
+        const worker = new Worker(workerUrl, { workerData });
+
+        worker.on('message', (result) => resolve( { status: 'resolved', data: result } ));
+        worker.on('error', () => resolve({ status: 'rejected', data: null }));
+    });
+
+    for (let i = 0; i < numCores; i++) {
+        workers.push(calculateFibonacci(startIndex + i));
+    }
+    const results = await Promise.all(workers);
+    console.log(results);
 };
 
 await performCalculations();
