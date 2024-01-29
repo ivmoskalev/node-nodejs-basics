1e37dceda454a414d807c0a5f3d3fae783399159
Igor Moskalev <moskalev.igor.v@gmail.com>
2024-01-30 03:15:14 +0600

    feat: add sha256 hash calculation for a file using streams api

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,31 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const filename = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    return new Promise((resolve, reject) => {
+        const hash = createHash('sha256');
+        const stream = createReadStream(filename);
+
+        stream.on('data', (chunk) => {
+            hash.update(chunk)
+        });
+
+        stream.on('end', () => {
+            const hexHash = hash.digest('hex');
+            console.log(hexHash);
+            resolve(hexHash);
+        });
+
+        stream.on('error', (err) => {
+            reject(err)
+        });
+    })
 };
 
 await calculateHash();
