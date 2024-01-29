49adf57f0e395acc1e0cd8cc68107469a97b3c7c
Igor Moskalev <moskalev.igor.v@gmail.com>
2024-01-30 00:47:20 +0600

    feat: add file operations (create, copy, rename, delete, list, read) using fs/promises

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import { cp } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+
 const copy = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const sourcePath = join(__dirname, 'files');
+    const targetPath = join(__dirname, 'files_copy');
+
+    try {
+        await cp(sourcePath, targetPath, {recursive: true, errorOnExist: true, force: false});
+        console.log('Files copied successfully');
+    }
+    catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await copy();
