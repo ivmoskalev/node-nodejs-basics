90e5c75e8155819050970e033e1e43d84fedcce2
Igor Moskalev <moskalev.igor.v@gmail.com>
2025-10-27 00:57:08 +0500

    feat: implement worker threads and child processes operations

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import { Worker } from 'node:worker_threads';
+import { availableParallelism } from 'node:os';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const performCalculations = async () => {
-  // Write your code here
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const workerPath = join(__dirname, 'worker.js');
+    const numCores = availableParallelism();
+
+    const workerPromises = Array.from({ length: numCores }, (_, i) => {
+        return new Promise((resolve) => {
+            const worker = new Worker(workerPath, { workerData: 10 + i });
+
+            worker.on('message', (data) => {
+                resolve({ status: 'resolved', data });
+            });
+
+            worker.on('error', () => {
+                resolve({ status: 'error', data: null });
+            });
+
+            worker.on('exit', (code) => {
+                if (code !== 0) resolve({ status: 'error', data: null });
+            });
+        });
+    });
+
+    const finalResults = await Promise.all(workerPromises);
+    console.log(finalResults);
 };
 
 await performCalculations();
